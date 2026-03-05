7d5f58c83d784da8b9abb68f2283a0ad656408cf
sangram-jr <sangrambera58@gmail.com>
2026-03-05 20:39:12 +0530

    add signin and logout functionality in FE

--- next-auth/app/page.tsx
+++ next-auth/app/page.tsx
@@ -1,9 +1,24 @@
-
+"use client"
+import { SessionProvider, signIn, signOut, useSession } from "next-auth/react";
 
 export default function Home() {
   return (
-    <div>
-      hi there
-    </div>
+    <SessionProvider>
+      <AnotherHome/>
+    </SessionProvider> 
   );
+}
+
+
+function AnotherHome(){
+  const session = useSession();
+  return (
+     <div>
+       {/* If the user is authenticated, show a sign-out button */}
+       {session.status === "authenticated" && <button onClick={() => signOut}>Sign Out</button>}
+
+       {/* If the user is unauthenticated, show a sign-in button */}
+       {session.status === "unauthenticated" && <button onClick={() => signIn()}>Sign In</button>}
+     </div>
+   );
 }
